02a5170cd23d3a6ba20744f3d9b82715c69e69f2
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-10-17 09:32:55 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+const authRoutes = require('./routes/AuthRoutes');
+const taskRoutes = require('./routes/TaskRoutes');
+
+app.use('/auth', authRoutes);
+app.use('/tasks', taskRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ routes/TaskRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/TaskController');
+
+// Routes de gestion des tâches
+// router.post('/create', taskController.createTask);
+// router.get('/list', taskController.listTasks);
+// router.get('/filter', taskController.filterTasks);
+// router.put('/update/:taskId', taskController.updateTask);
+// router.delete('/delete/:taskId', taskController.deleteTask);
+
+module.exports = router;
