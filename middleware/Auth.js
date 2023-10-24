63cd5400864b863199f667c1e3a993151346b4b5
Emilie <92803520+SakoSuta@users.noreply.github.com>
2023-10-24 12:02:33 +0200

    new middleware

---
+++ middleware/Auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const AuthenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({ error: "Authentication token is missing." });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid or expired token." });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = AuthenticateToken;
--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/TaskController');
+const AuthenticateToken = require('../middleware/Auth');
 
 // Routes de gestion des tâches
-// router.post('/create', taskController.createTask);
+router.post('/create', AuthenticateToken, taskController.createTask);
 // router.get('/list', taskController.listTasks);
 // router.get('/filter', taskController.filterTasks);
 // router.put('/update/:taskId', taskController.updateTask);
